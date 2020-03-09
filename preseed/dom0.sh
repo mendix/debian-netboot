cbed5ae5c0296cb65bb266b38efbd0f8f145999d
Igor Yurchenko <igor.yurchenko@mendix.com>
2020-03-09 11:31:00 +0100

    dom0.sh: remove Daniel's key

--- preseed/dom0.sh
+++ preseed/dom0.sh
@@ -4,7 +4,6 @@
 /bin/cat << EOF > /root/.ssh/authorized_keys
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIpI3OaNyY8S2jDg4ibT4hRiQwzZTslrysKr1epZEWfh hans.van.kranenburg@mendix.nl
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAje0ghkW9j9yLuidajJZoIvhlKvpodnW+h2jyFPUC/k pim.van.den.berg@mendix.com
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDdUTmGM3TECPzdDl58kLNtQB2V5f5tNosPjT0vdg6qb daniel.van.dorp@mendix.com
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJXA5i/JZf3HDFsxb+zH//uoCBmRyqLS8bPIh/1dncNb maarten.hendrix@mendix.com
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIISKPx8BrmQf8MSMSqRQANhjDQszH/TI3zMcTb05RjXk igor.yurchenko@mendix.com
 EOF
