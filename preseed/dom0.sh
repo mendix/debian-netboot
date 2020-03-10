4d8c8a7a72bae406127ea862fa0b586c027feece
Igor Yurchenko <igor.yurchenko@mendix.com>
2020-03-10 11:15:48 +0100

    dom0.sh: fixup (excessive quotes)
    We missed extra quotes in ssh key lines in the previous commits

--- preseed/dom0.sh
+++ preseed/dom0.sh
@@ -6,9 +6,9 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIpI3OaNyY8S2jDg4ibT4hRiQwzZTslrysKr1epZEWfh
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAje0ghkW9j9yLuidajJZoIvhlKvpodnW+h2jyFPUC/k pim.van.den.berg@mendix.com
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJXA5i/JZf3HDFsxb+zH//uoCBmRyqLS8bPIh/1dncNb maarten.hendrix@mendix.com
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIISKPx8BrmQf8MSMSqRQANhjDQszH/TI3zMcTb05RjXk igor.yurchenko@mendix.com
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaD3WThJzRzA5GND8m/ztxfWH3T/Ns2XL1h34ZFYp93 cloudops@baron'
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILLMc+s2eUMmc5aSjNGwUNPu8UvQFnO347Gp6+iI8ogP cloudops@bofh'
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINhTs2ws1y9tD+0QllQCaSp9CfpnAUfSSCTPmvEzgEgu cloudops@pino'
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaD3WThJzRzA5GND8m/ztxfWH3T/Ns2XL1h34ZFYp93 cloudops@baron
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILLMc+s2eUMmc5aSjNGwUNPu8UvQFnO347Gp6+iI8ogP cloudops@bofh
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINhTs2ws1y9tD+0QllQCaSp9CfpnAUfSSCTPmvEzgEgu cloudops@pino
 EOF
 
 if /bin/grep -q ^8 /etc/debian_version
