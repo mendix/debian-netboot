7040c06daca45fa3a851aab91b46a40307c6579f
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2021-02-01 15:22:05 +0100

    preseed: add Bart to ssh access

--- preseed/dom0.sh
+++ preseed/dom0.sh
@@ -6,6 +6,7 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIpI3OaNyY8S2jDg4ibT4hRiQwzZTslrysKr1epZEWfh
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAje0ghkW9j9yLuidajJZoIvhlKvpodnW+h2jyFPUC/k pim.van.den.berg@mendix.com
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJXA5i/JZf3HDFsxb+zH//uoCBmRyqLS8bPIh/1dncNb maarten.hendrix@mendix.com
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIISKPx8BrmQf8MSMSqRQANhjDQszH/TI3zMcTb05RjXk igor.yurchenko@mendix.com
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMO5lhA8dr+RbU7HF6hrSDvQ92oN7mWohjmDCsu4XKYY bart.van.der.meijden@mendix.com
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaD3WThJzRzA5GND8m/ztxfWH3T/Ns2XL1h34ZFYp93 cloudops@baron
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILLMc+s2eUMmc5aSjNGwUNPu8UvQFnO347Gp6+iI8ogP cloudops@bofh
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINhTs2ws1y9tD+0QllQCaSp9CfpnAUfSSCTPmvEzgEgu cloudops@pino
